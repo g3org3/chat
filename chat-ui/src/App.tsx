2316982e31de8d707159e375f4ce511d5915e2a9
george <7jagjag@gmail.com>
2023-05-21 15:36:49 +0200

    generate front with vitejs + typescript + react

---
+++ chat-ui/src/App.tsx
@@ -0,0 +1,13 @@
+import viteLogo from '/logo.svg'
+
+function App() {
+
+  return (
+    <main>
+      <img src={viteLogo} className="logo" alt="Vite logo" />
+      <h1>JA's Chat App</h1>
+    </main>
+  )
+}
+
+export default App
