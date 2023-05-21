09b424bf03b89b55b801fdbcc7f2b0d1fd754783
george <7jagjag@gmail.com>
2023-05-21 15:53:37 +0200

    :lipstick: created the nav

--- chat-ui/src/App.tsx
+++ chat-ui/src/App.tsx
@@ -1,11 +1,14 @@
 import viteLogo from '/logo.svg'
 
 function App() {
-
   return (
-    <main>
-      <img src={viteLogo} className="logo" alt="Vite logo" />
-      <h1>JA's Chat App</h1>
+    <main className="bg-gray-100 h-screen flex flex-col">
+      <nav className="bg-white h-[48px] shadow flex flex-col">
+        <div className='container mx-auto flex px-3 items-center gap-2 flex-1'>
+          <img src={viteLogo} className="h-8" alt="Vite logo" />
+          <span className="text-purple-800 text-lg font-bold">Chat App</span>
+        </div>
+      </nav>
     </main>
   )
 }
